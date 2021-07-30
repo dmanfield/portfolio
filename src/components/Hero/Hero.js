df1bb564dda75d1be441898ddd9a2a606ab8a3bb
Daniel Manfield <dmanfield@williamhill.us>
2021-07-29 21:03:31 -0400

    Day 1

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Welcome To<br/>
+      My Personal Portfolio
+    </SectionTitle>
+    <SectionText>
+      Here to show my awesome projects!
+    </SectionText>
+    <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+  </LeftSection>
+</Section>
+);
+
+export default Hero;
